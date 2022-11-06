d308946bbed47a3e82258723c02c9ec022d34d45
menulln <39300523+menulln@users.noreply.github.com>
2022-11-06 19:16:11 +0100

    Move grid render process into new function renderGrid()

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 const container = document.querySelector('.container');
 const containerSize = 800;
 
-for (let i = 0; i < 1786; i++) {
-    const gridElement = document.createElement('div');
-    gridElement.style.cssText = (` width: 15px; 
-                                   height: 15px; 
-                                   border: 1px solid black;`);
-    gridElement.classList.toggle('grid');
-    container.appendChild(gridElement);
+renderGrid();
+
+function renderGrid() {
+    for (let i = 0; i < 1786; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.style.cssText = (` width: 15px; 
+                                       height: 15px; 
+                                       border: 1px solid black;`);
+        gridElement.classList.toggle('grid');
+        container.appendChild(gridElement);
+    }
+    
+    const gridElements = document.querySelectorAll('.grid');
+    
+    gridElements.forEach(element => {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = 'black';
+        });
+    });
 }
 
-const gridElements = document.querySelectorAll('.grid');
-
-gridElements.forEach(element => {
-    element.addEventListener('mouseover', () => {
-        element.style.backgroundColor = 'black';
-    });
-});
