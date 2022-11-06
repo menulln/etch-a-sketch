a077f0e9ae895d7ffd0fe96a694b76da0b2a0fcb
menulln <39300523+menulln@users.noreply.github.com>
2022-11-06 18:40:53 +0100

    Adjust border settings on grid element

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@ for (let i = 0; i < 1000; i++) {
     const gridElement = document.createElement('div');
     gridElement.style.cssText = (` width: 15px; 
                                    height: 15px; 
-                                   border-bottom: 1px solid black; 
-                                   border-right: 1px solid black;`);
+                                   border: 1px solid black;`);
     gridElement.classList.toggle('grid');
     container.appendChild(gridElement);
 }
