7028aa2cb92ac60aa0894b24fc2e7c3c6c4200d5
menulln <39300523+menulln@users.noreply.github.com>
2022-11-06 19:34:00 +0100

    Change renderGrid() function to use variable values

--- js/script.js
+++ js/script.js
@@ -8,16 +8,18 @@ buttonSquares.addEventListener('click', () => {
     const squaresPerSide = +prompt('Set number of squares per side. (1-100)');
 });
 
-function renderGrid() {
-    for (let i = 0; i < 1786; i++) {
-        const gridElement = document.createElement('div');
-        gridElement.style.cssText = (` width: 15px; 
-                                       height: 15px; 
-                                       border: 1px solid black;`);
-        gridElement.classList.toggle('grid');
-        container.appendChild(gridElement);
+function renderGrid(squaresPerSide = 24) {
+    for (let i = 0; i < squaresPerSide; i++) { 
+        for (let j = 0; j < squaresPerSide; j++) {
+            const gridElement = document.createElement('div');
+            gridElement.style.cssText = (` width: ${(containerSize / squaresPerSide) - 2}px; 
+                                        height: ${(containerSize / squaresPerSide) - 2}px; 
+                                        border: 1px solid black;`);
+            gridElement.classList.toggle('grid');
+            container.appendChild(gridElement);
+        }
     }
-    
+
     const gridElements = document.querySelectorAll('.grid');
     
     gridElements.forEach(element => {
